da778882567a99371d231223b524d176deec740e
Benjamin Toll <benjam72@yahoo.com>
2016-04-22 19:24:57 -0400

    Preparing Stank to only have its type functions mixed in to the base

--- src/visitor.js
+++ src/visitor.js
@@ -1,17 +1,46 @@
 /* eslint-disable no-case-declarations,one-var */
 'use strict';
 
-// NOTE:
-// We wrap the node in a context object so we can specify our own custom node types like
-// 'TooManyReturns', etc.  It's better to do this than to modify objects we don't own,
-// which is no bueno on its own merits, and also because it can lead to headaches since
-// we're collecting references NOT copies.
-//
-//        results.push({
-//            node,
-//            type: 'TooManyReturns'
-//        });
-//
+const captureBinary = (node, parent, results) => {
+    // TODO: Optimize.
+    if (['instanceof', 'typeof'].indexOf(node.operator) > -1) {
+        results.push({
+            node: parent,
+            type: node.operator
+        });
+    }
+};
+
+const captureFunction = function (node, parent, results) {
+    const bodies = node.body.body;
+
+    // TODO: Make .length configurable.
+    if (bodies && bodies.length > 10) {
+        results.push({
+            node,
+            type: 'TooManyFunctionBodyStatements'
+        });
+    }
+
+    stackManager.push([0]);
+
+    bodies.forEach(n => this.visit(n, node, results));
+
+    const a = stackManager.pop();
+
+    if (a[0] > 1) {
+        results.push({
+            node,
+            type: 'TooManyReturns'
+        });
+    }
+};
+
+const captureLoop = (node, parent, results) =>
+    results.push({
+        node: parent || node,
+        type: node.type
+    });
 
 const stackManager = (() => {
     const stack = [];
@@ -29,138 +58,45 @@ const stackManager = (() => {
 })();
 
 module.exports = {
-    visit: function (node, parent, results) {
-        switch (node.type) {
-            case 'ArrowFunctionExpression':
-            case 'FunctionDeclaration':
-            case 'FunctionExpression':
-                const bodies = node.body.body;
+    ArrowFunctionExpression(node, parent, results) {
+        captureFunction.call(this, node, parent, results);
+    },
+    FunctionDeclaration(node, parent, results) {
+        captureFunction.call(this, node, parent, results);
+    },
+    FunctionExpression(node, parent, results) {
+        captureFunction.call(this, node, parent, results);
+    },
 
-                // TODO: Make .length configurable.
-                if (bodies && bodies.length > 10) {
-                    results.push({
-                        node,
-                        type: 'TooManyFunctionBodyStatements'
-                    });
-                }
+    BinaryExpression: captureBinary,
+    UnaryExpression: captureBinary,
 
-                stackManager.push([0]);
-
-                bodies.forEach(n => this.visit(n, node, results));
-
-                const a = stackManager.pop();
-
-                if (a[0] > 1) {
-                    results.push({
-                        node,
-                        type: 'TooManyReturns'
-                    });
-                }
-                break;
-
-            case 'AssignmentExpression':
-                this.visit(node.right, node, results);
-                break;
-
-            case 'BinaryExpression':
-            case 'UnaryExpression':
-                // TODO: Optimize.
-                if (['instanceof', 'typeof'].indexOf(node.operator) > -1) {
-                    results.push({
-                        node: parent,
-                        type: node.operator
-                    });
-                }
-                break;
-
-            case 'BlockStatement':
-                node.body.forEach(node => this.visit(node, parent, results));
-                break;
-
-            case 'CallExpression':
-                // TODO
-                const callArgs = node.arguments;
-
-                if (callArgs.length) {
-                    callArgs.forEach(node => {
-                        // TODO: Test this logic!
-                        if (node.type !== 'Identifier') {
-                            this.visit(node, node, results);
-                        }
-                    });
-                } else if (node.callee) {
-                    this.visit(node.callee, node, results);
-                }
-                break;
-
-            case 'ConditionalExpression':
-                if (node.consequent.type === 'ConditionalExpression' ||
-                    node.alternate.type === 'ConditionalExpression') {
-                    results.push({
-                        node,
-                        type: 'NestedTernaryOperators'
-                    });
-                }
-                break;
-
-            case 'ExpressionStatement':
-                this.visit(node.expression, node, results);
-                break;
-
-            case 'ForStatement':
-            case 'ForInStatement':
-            case 'ForOfStatement':
-            case 'Identifier':
-            case 'NewExpression':
-            case 'WhileStatement':
-                results.push({
-                    node: parent || node,
-                    type: node.type
-                });
-                break;
-
-            case 'IfStatement':
-                this.visit(node.consequent, node, results);
-
-                if (node.alternate) {
-                    this.visit(node.alternate, node, results);
-                }
-                break;
-
-            case 'ObjectExpression':
-                node.properties.forEach(node => this.visit(node, parent, results));
-                return results;
-
-            case 'Program':
-                node.body.forEach(node => this.visit(node, parent, results));
-                return results;
-
-            case 'Property':
-                this.visit(node.value, parent, results);
-                return results;
-
-            case 'ReturnStatement':
-                const returnArgs = node.argument.arguments;
-
-                if (returnArgs) {
-                    returnArgs.forEach(node => this.visit(node, parent, results));
-                }
-
-                stackManager.incr();
-                break;
-
-            case 'VariableDeclarator':
-                const init = node.init;
-
-                if (init) {
-                    this.visit(init, node, results);
-                }
-                break;
-
-            case 'VariableDeclaration':
-                node.declarations.forEach(node => this.visit(node, parent, results));
-                break;
+    ConditionalExpression(node, parent, results) {
+        if (node.consequent.type === 'ConditionalExpression' ||
+            node.alternate.type === 'ConditionalExpression') {
+            results.push({
+                node,
+                type: 'NestedTernaryOperators'
+            });
         }
+    },
+
+    ForStatement: captureLoop,
+    ForInStatement: captureLoop,
+    ForOfStatement: captureLoop,
+    DoWhileStatement: captureLoop,
+    Identifier: captureLoop,
+    NewExpression: captureLoop,
+    WhileStatement: captureLoop,
+
+    ReturnStatement(node, parent, results) {
+        const returnArgs = node.argument.arguments;
+
+        if (returnArgs) {
+            returnArgs.forEach(node => this.visit(node, parent, results));
+        }
+
+        stackManager.incr();
     }
 };
 
